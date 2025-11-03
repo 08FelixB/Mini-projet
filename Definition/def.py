68f654a7338f080e39c283bb9412d8975071947a
Adem777777 <2580776@cegepotaouais.qc.ca>
2025-11-03 11:35:29 -0500

    afficher cases def (besoin de parler pour changer dess choses sur les valeurs

--- Definition/def.py
+++ Definition/def.py
@@ -1,4 +1,16 @@
+def afficher_cases(tour_joueur):
+    """
 
+    :param tour_joueur: le tour de quel joueur
+    :return:
+    """
+    if tour_joueur == "Joueur 1":
+        print("Liste du Joueur 1, placeholder")
+    else:
+        print("Liste du Joueur 2, placeholder")
+        """
+        TODO: mettre les vraies listes dans les print, valeur utilisée peut etre changée dependadament sur quoi on choisit
+        """
 
 
 
