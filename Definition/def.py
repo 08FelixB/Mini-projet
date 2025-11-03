76f12f2b5629f24897e95395d382fc1a84287ee0
08FelixB <2556052@cegepoutaouais.qc.ca>
2025-11-03 12:00:53 -0500

    ajout de pseudocode et de fonction

--- Definition/def.py
+++ Definition/def.py
@@ -1,4 +1,69 @@
-.
+
+def afficher_cases(tour_joueur):
+    """
+
+    :param tour_joueur: le tour de quel joueur
+    :return:
+    """
+    if tour_joueur == "Joueur 1":
+        print("Liste du Joueur 1, placeholder")
+    else:
+        print("Liste du Joueur 2, placeholder")
+        """
+        TODO: mettre les vraies listes dans les print, valeur utilisée peut etre changée dependadament sur quoi on choisit
+        """
+
+
+
+
+
+
+
+
+
+
+def supprime_bateau():
+    """
+    Fonction qui supprime un bateau atteint complètement
+    :return: None
+    """
+    #Si un bateaux a été atteint complètement, il disparait
+
+
+def affiche_nombre_bateau_restant():
+    """
+    Fonction qui affiche le nombre de bateau restant après chaque bateau noyé
+    :return: Le nombre de bateau restant du j1 ou du j2
+    """
+    #Si un des bateaux du j1 a été atteint complètement, affiche le nombre de bateau restant du j1
+
+
+
+
+    #Si un des bateaux du j2 a été atteint complètement, affiche le nombre de bateau restant du j2
+
+
+    #Si le nombre de bateau restant du j1 ou du j2 est 0, retourne le gagnant
+
+
+
+
+
+
+
+def afficher_gagnant():
+  """
+  Fonction qui affiche le gagnant
+  :return: Le nom du gagnant
+  """
+  #Si le nombre de bateaux du j1 est plus petit que le nombre de bateaux du j2, le gagnant est j2
+
+
+  #Si le nombre de bateaux du j2 est plus petit que le nombre de bateaux du j1, le gagnant est j1
+
+
+
+
 
 
 
